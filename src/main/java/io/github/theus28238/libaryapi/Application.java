697bdb89dc4238fd7a6e54c82893917cd337a8d6
Theus28238 <matheusguilherme1520@gmail.com>
2025-06-17 16:23:16 -0300

    fazendo pool de conexões utilizando o Hikari

---
+++ src/main/java/io/github/theus28238/libaryapi/Application.java
@@ -0,0 +1,13 @@
+package io.github.theus28238.libaryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
